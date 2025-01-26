59533b20703e54f898e531e8e51e59c43d235df9
rahulkr099 <techaffairs4@gmail.com>
2025-01-26 21:57:06 +0530

    dffd

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
 // constants.js
-export const BASEURL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000/api/v1';
-export const URL = import.meta.env.VITE_URL || 'http://localhost:4000';
+export const BASEURL = 'https://shortly-six-beta.vercel.app/'
+export const URL = 'https://shortly-six-beta.vercel.app/';
