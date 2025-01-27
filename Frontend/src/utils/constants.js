ca2f8d64c707675b58538746716e60d9f1c5c9ae
rahulkr099 <techaffairs4@gmail.com>
2025-01-27 09:36:32 +0530

    qqq

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,3 +1,7 @@
 // constants.js
-export const BASEURL = 'https://shortly-six-beta.vercel.app/api/v1'
-export const URL = 'https://shortly-six-beta.vercel.app';
+// export const BASEURL = 'https://shortly-six-beta.vercel.app/api/v1'
+// export const URL = 'https://shortly-six-beta.vercel.app';
+// export const BASEURL = 'http://localhost:4000/api/v1'
+// export const URL = 'http://localhost:4000';
+export const BASEURL = 'https://shortly-klp2.onrender.com/api/v1'
+export const URL = 'https://shortly-klp2.onrender.com';
